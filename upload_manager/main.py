dcfbbac04cb05cebcecd4408654d13ee55190c81
arashmad <arashmad@gfz-potsdam.de>
2023-08-04 12:15:00 +0200

    some notes for nex TODOs

--- upload_manager/main.py
+++ upload_manager/main.py
@@ -2,11 +2,11 @@ import os
 import zipfile
 
 workspace_dir = 'workspace_dir'
-file_path = 'inhouse_map_app/test/data/Polygon.zip' 
+file_path = 'inhouse_map_app/test/data/Polygon.zip'
 VALID_FORMATS = ['shp', 'shx', 'dbf', 'prj']
 
 
-def decompress_zip_file(input_path:str, output_path: str=''):
+def decompress_zip_file(input_path: str, output_path: str = '') -> str:
     """
     This function decompress the ESRI archived file.
 
@@ -23,30 +23,45 @@ def decompress_zip_file(input_path:str, output_path: str=''):
         path to the decompress ESRI archived file
     """
     try:
-        if not output_path :
+        # TODO
+        # check file exist and raise FileNotFoundException() if it's necessary
+        if not os.path.isfile(input_path):
+            pass
+
+        if not output_path:
             output_path = os.path.join(workspace_dir, 'test1')
 
         file_format = os.path.basename(input_path).split('.')[-1]
         file_name = os.path.basename(input_path).split('.')[0]
+
+        # TODO
+        # Better coding for if else
         if file_format == 'zip':
             zipfile_size = os.path.getsize(input_path)
             if zipfile_size <= 500000:
-                with zipfile.ZipFile(input_path, 'r') as archived:
+                with zipfile.ZipFile(input_path) as archived:
                     # TODO
                     # I need to check .msi file or .exe .sh .bash
                     # file_contents = archived.namelist()
                     # for item in file_contents:
                     #     if item is valid_format:
                     #         pass
-                    archived.extractall(output_path)
-                    
 
-                
+                    # TODO
+                    # Not working
+                    archived.extractall(output_path)
+
+                    return (output_path)
+
             else:
-                raise('file size is bigger than 5 Mb')
+                # TODO
+                # Fix the message
+                raise Exception('file size is bigger than 5 Mb')
 
         else:
-            raise('this file is not a zipfile', zipfile.BadZipFile)
+            raise Exception(
+                f'Use .zip format as input => format (.{file_format}) is not supported.')
 
     except Exception as e:
-        raise('failed to decompress the ESRI archived file. => ', str(e))
+        raise Exception(
+            'Failed to decompress the ESRI archived file. => ', str(e))
